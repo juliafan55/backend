888e8a4de05f038329193cd34a449b06917caa71
juliafan55 <juliafan55@gmail.com>
2022-06-10 15:47:57 -0400

    friend backend done

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, activateAccount, login, auth, getProfile } = require("../controllers/user")
+const { register, activateAccount, login, auth, getProfile, addFriend, cancelRequest, follow, unfollow, acceptRequest, unfriend, deleteRequest } = require("../controllers/user")
 const { authUser } = require("../middlewares/auth")
 
 router.post("/register", register)
@@ -9,6 +9,13 @@ router.post("/activate", activateAccount)
 router.post("/login", login)
 router.post("/auth", authUser, auth)
 router.get("/getProfile/:username", authUser, getProfile)
+router.put("/addFriend/:id", authUser, addFriend)
+router.put("/cancelRequest/:id", authUser, cancelRequest)
+router.put("/follow/:id", authUser, follow)
+router.put("/unfollow/:id", authUser, unfollow)
+router.put("/acceptRequest/:id", authUser, acceptRequest)
+router.put("/unfriend/:id", authUser, unfriend)
+router.put("/deleteRequest/:id", authUser, deleteRequest)
 
 
 module.exports = router;
