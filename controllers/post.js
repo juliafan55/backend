ac68c01778f58ceb52550bd6b1b6aaf2129312fb
juliafan55 <juliafan55@gmail.com>
2022-06-11 19:20:47 -0400

    delete posts

--- controllers/post.js
+++ controllers/post.js
@@ -53,3 +53,12 @@ exports.comment = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+exports.deletePost = async (req, res) => {
+  try {
+    await Post.findByIdAndRemove(req.params.id)
+    res.json({status:"ok"})
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { createPost, getAllPosts, comment } = require("../controllers/post")
+const { createPost, getAllPosts, comment, deletePost } = require("../controllers/post")
 const { authUser } = require("../middlewares/auth")
 
 router.post("/createPost", authUser, createPost)
 router.get("/getAllPosts", authUser, getAllPosts)
 router.put("/comment", authUser, comment)
+router.delete("/deletePost/:id", authUser, deletePost)
 
 
 module.exports = router;
