4e09b6a4b06d9d3e1975b8249554fcbbf84c4802
juliafan55 <juliafan55@gmail.com>
2022-06-12 13:09:16 -0400

    update profile picture

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, activateAccount, login, auth, getProfile, addFriend, cancelRequest, follow, unfollow, acceptRequest, unfriend, deleteRequest } = require("../controllers/user")
+const { register, activateAccount, login, auth, getProfile, addFriend, cancelRequest, follow, unfollow, acceptRequest, unfriend, deleteRequest, updateProfilePicture } = require("../controllers/user")
 const { authUser } = require("../middlewares/auth")
 
 router.post("/register", register)
@@ -16,6 +16,7 @@ router.put("/unfollow/:id", authUser, unfollow)
 router.put("/acceptRequest/:id", authUser, acceptRequest)
 router.put("/unfriend/:id", authUser, unfriend)
 router.put("/deleteRequest/:id", authUser, deleteRequest)
+router.put("/updateProfilePicture", authUser, updateProfilePicture)
 
 
 module.exports = router;
